b47d0150d264003b431e575bbbe5fb6e893b0a40
skaer2 <max.bog200174@gmail.com>
2020-02-14 12:07:48 +0300

    added more grammar definitions

--- src/PythonParser.hs
+++ src/PythonParser.hs
@@ -1,9 +1,17 @@
 import Control.Applicative ((<|>))
 
 -- Code = List Actions
+--
 -- Actions = Assignment | Function | Call | Keywords
--- Assignment = Name Assign Expr
+--
+-- Assignment = Name Assign Expr 
+--              | Name BinOperation Assign Expr
+--
+-- Assign = '='
+-- Name = String starting with a letter
+--
 -- Expr = Const | Expr BinOperation Expr | UnOperation Expr
+--
 -- BinOperation = Add | Substract 
 --          | Mult | Divide 
 --          | Div | Mod 
@@ -11,5 +19,24 @@ import Control.Applicative ((<|>))
 --          | Equal | More | Less 
 --          | MoreOrEqual | LessOrEqual 
 --          | NotEqual 
--- UnOperation = 
+--          | Or | And
 --
+-- Add = '+'
+-- Substract = '-'
+--
+-- UnOperation = Not          --Also Operators on bits
+--
+--
+-- Function = Def Name '(' Arguments ')' ':' 
+--            BlockBegin Code BlockEnd
+-- BlockBegin = IndentIn
+-- BlockEnd   = IndentOut
+--
+
+data Code = Code [Actions]
+    deriving (Show, Eq)
+
+data Actions = Action Assignment
+
+data Assignment = Assignment Name Expr
+
